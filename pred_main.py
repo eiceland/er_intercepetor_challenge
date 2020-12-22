60fd4a80e552d7c288edace8edccd0adae170d06
ERAN ICELAND <ERANICE@rf.local>
2020-12-22 21:45:11 +0200

    improving performance of interception

--- pred_main.py
+++ pred_main.py
@@ -7,6 +7,8 @@ LEFT = 0
 STRAIGHT = 1
 RIGHT = 2
 SHOOT = 3
+
+
 #
 # #given two succesive points in a path, compute the next n points.
 # def get_path(x0, y0, x1, y1, n, fric = 5e-7, dt = 0.2 , g = 9.8):
@@ -164,69 +166,73 @@ SHOOT = 3
 #     print(t_and_ang)
 
 # def my_unitests():
-    # check_path_n_steps(10)
-    # check_full_path_and_city_hit()
-    # this function might fail if the interceptor did not complete its path (since it met a rocket...)
-    # check_interceptor_path()
-    # check_interception_points()
+# check_path_n_steps(10)
+# check_full_path_and_city_hit()
+# this function might fail if the interceptor did not complete its path (since it met a rocket...)
+# check_interceptor_path()
+# check_interception_points()
 
 class MyRocket():
-    def __init__(self, x1, y1, cur_time, city_list, intr_path_dict):
+    def __init__(self, x1, y1, cur_time, city_list, intr_paths_mat):
         self.path = []
         self.city_hit = False
-        self.interception_points = [] #each interception point holds: inteceptor shooting time, angle, expected interception time
+        self.interception_points = []  # each interception point holds: inteceptor shooting time, angle, expected interception time
         self.city_list = city_list
         self.x0 = 4800.0
         self.y0 = 0.0
         self.x1 = x1
         self.y1 = y1
-        self.shoot_time = cur_time
-        self.intr_path_dict = intr_path_dict
+        self.shoot_time = cur_time - 2 #r_locs represents the situation in time cur_time-1.
+        self.intr_paths_mat = intr_paths_mat
+        self.index_2_ang = {i: -90 + 6 * i for i in range(31)}
+
         self.update_rocket_path_and_city_hit()
         self.calculate_interception_points()
 
-    def update_rocket_path_and_city_hit(self, fric = 5e-7, dt = 0.2 , g = 9.8):
+    def update_rocket_path_and_city_hit(self, fric=5e-7, dt=0.2, g=9.8):
         dx, dy = self.x1 - self.x0, self.y1 - self.y0
         vx, vy = dx / dt, dy / dt
-        self.path.append([self.x0, self.y0, self.shoot_time ])
+        self.path.append([self.x0, self.y0, self.shoot_time])
         x, y, t = self.x1, self.y1, self.shoot_time + 1
-        self.path.append([x, y, t])
         while y >= 0:
+            self.path.append([x, y, t])
             t += 1
             v_loss = (vx ** 2 + vy ** 2) * fric * dt
             vx = vx * (1 - v_loss)
             vy = vy * (1 - v_loss) - g * dt
             x = x + vx * dt
             y = y + vy * dt
-            self.path.append([x, y, t])
 
         for c in self.city_list:
-            if np.abs(x - c[0]) < c[1]: # c[0] = x coordinate of city center, c[1] = distance from city center to the edge
+            if np.abs(x - c[0]) < c[1]:  # c[0] = x coordinate of city center, c[1] = distance from city center to the edge
                 self.city_hit = True
 
-    def calculate_interception_points(self, prox_radius = 150):
-        for t in range(len(self.path)):
-            for ang in range(-90, 91, 6):
-                intr_path = self.intr_path_dict[ang]
-                r_path_t = np.array(self.path[t:])
-                min_len = min(intr_path.shape[0], r_path_t.shape[0])
-                squared_dif = (intr_path[:min_len, :2] - r_path_t[:min_len, :2]) ** 2
-                dists_squared = squared_dif[:, 0] + squared_dif[:, 1]
-                if (dists_squared < prox_radius ** 2).any():
-                    intr_time = np.argmin(dists_squared)
-                    self.interception_points.append([self.shoot_time+t, ang, self.shoot_time + t + intr_time])
+    def calculate_interception_points(self, prox_radius=150):
+        for t in range(2, len(self.path)): #the first two locations are in the past.
+            r_path_t = np.array(self.path[t:])
+            min_len = min(self.intr_paths_mat.shape[1], r_path_t.shape[0])
+            squared_dif = (self.intr_paths_mat[:, :min_len, :] - r_path_t[:min_len, :2]) ** 2
+            dists_squared = squared_dif[:, :, 0] + squared_dif[:, :, 1]
+            meeting_points = np.nonzero(dists_squared < prox_radius ** 2)
+            last_index = -1
+            for index, time in zip(*meeting_points):
+                if last_index == index:
+                    continue
+                last_index = index
+                self.interception_points.append(
+                    [self.shoot_time + t, self.index_2_ang[index], self.shoot_time + t + time])
+
 
 class MyInterceptor():
     def __init__(self):
-        self.r_city_list = []
-        self.r_field_list = []
+        self.rockets_list = []
         self.strategy = None
         self.time = 0
-        self.intr_path_dict = {ang: np.array(self.get_interceptor_path(ang)[:-1]) for ang in range(-90, 91, 6)}
-        self.t_no_new_city_r = 0
-        self.t_no_new_field_r = 0
+        self.t_no_new_rocket = 0
+        self.intr_paths_mat = self.get_interceprtion_mat()
 
-    def get_interceptor_path(self, ang0, v0 = 800, x0 = -2000, y0 = 0, world_width = 10000, fric = 5e-7, dt = 0.2 , g = 9.8):
+    def get_interceptor_path(self, ang0, v0=800, x0=-2000.0, y0=0.0, world_width=10000, fric=5e-7, dt=0.2, g=9.8):
+        max_path_len = 353
         vx = v0 * np.sin(np.deg2rad(ang0))
         vy = v0 * np.cos(np.deg2rad(ang0))
         path = []
@@ -239,70 +245,69 @@ class MyInterceptor():
             x = x + vx * dt
             y = y + vy * dt
             path.append([x, y])
-        return path
+        while (len(path) < max_path_len + 1):
+            path.append([10000.0, 0.0])
+        return path[:max_path_len]
 
-    def is_new_rocket(self, x1,y1):
-        if (len(self.r_city_list) > 0):
-            expected_loc =  (self.r_city_list[-1]).path[self.t_no_new_city_r+1][:2]
-            if (np.abs(np.array(expected_loc) - np.array([x1,y1])) < 1e-6).all():
-                return False
-        if (len(self.r_field_list) > 0):
-            expected_loc =  (self.r_field_list[-1]).path[self.t_no_new_field_r+1][:2]
+    def get_interceprtion_mat(self):
+        paths = []
+        for ang in range(-90, 91, 6):
+            paths.append(self.get_interceptor_path(ang))
+        return np.array(paths)
+
+    def is_new_rocket(self, x1, y1):
+        if ((x1 - 4800.0)**2 + y1 **2) > 400**2:
+            return False
+        if self.t_no_new_rocket > 3:
+            return True
+        if (len(self.rockets_list) > 0):
+            expected_loc = (self.rockets_list[-1]).path[self.t_no_new_rocket + 1][:2]
             if (np.abs(np.array(expected_loc) - np.array([x1, y1])) < 1e-6).all():
                 return False
         return True
 
     def calculate_action(self, r_locs, i_locs, c_locs, ang, score, t):
 
-        self.t_no_new_city_r += 1
-        self.t_no_new_field_r += 1
-        if (len(r_locs)>0):
-            x1,y1 = r_locs[-1]
-        if (len(r_locs)>0) and (self.is_new_rocket(x1,y1)):
-            rocket = MyRocket(*r_locs[-1], t, c_locs, self.intr_path_dict )
-            if rocket.city_hit:
-                self.r_city_list.append(rocket)
-                self.t_no_new_city_r = 0
-            else:
-                self.r_field_list.append(rocket)
-                self.t_no_new_field_r = 0
+        self.time = t
+        self.t_no_new_rocket += 1
+        if (len(r_locs) > 0):
+            x1, y1 = r_locs[-1]
+        if (len(r_locs) > 0) and (self.is_new_rocket(x1, y1)):
+            rocket = MyRocket(*r_locs[-1], t, c_locs, self.intr_paths_mat)
+            self.rockets_list.append(rocket)
+            self.t_no_new_rocket = 0
 
-        #cur_game_map = game_map.build_game_map(self.t_and_ang_city_all, self.t_and_ang_field_all, ang)  ##Rafi
+        # cur_game_map = game_map.build_game_map(self.t_and_ang_city_all, self.t_and_ang_field_all, ang)  ##Rafi
 
-        while (self.strategy is None) and (len(self.r_city_list) > 0):
-            rocket_to_hit = self.r_city_list[0]
+        if (self.strategy is None) and (len(self.rockets_list) > 0):
+            rocket_to_hit = self.rockets_list[0]
             for p in rocket_to_hit.interception_points:
                 self.strategy = p
-                if ((p[0] - t)*6 > np.abs(p[1] - ang)): # p[0] absolute shooting time. p[1] angle to shoot
+                if ((p[0] - t) * 6 > np.abs(p[1] - ang)):  # p[0] absolute shooting time. p[1] angle to shoot
                     break
                 self.strategy = None
-            if self.strategy is None:
-                self.r_city_list.remove(rocket_to_hit)
 
         if self.strategy is None:
             if (ang > 45):
-                return RIGHT
+                action =  RIGHT
             else:
-                return LEFT
-        # here self.strategy is not None
-        if self.strategy[1] > ang:
-            action = RIGHT
-        elif self.strategy[1] < ang:
-            action = LEFT
-        elif self.strategy[0] == t:
-            action = SHOOT
-            self.strategy = None
-            self.r_city_list.remove(self.r_city_list[0])
+                action = LEFT
         else:
-            action = STRAIGHT
+            if self.strategy[1] > ang:
+                action = RIGHT
+            elif self.strategy[1] < ang:
+                action = LEFT
+            elif self.strategy[0] == t:
+                action = SHOOT
+                self.strategy = None
+                self.rockets_list.remove(self.rockets_list[0])
+            else:
+                action = STRAIGHT
 
         # remove rockets that landed
-        for r in self.r_city_list:
-            if r.path[-1][2] <= t:
-                self.r_city_list.remove(r)
-        for r in self.r_field_list:
-            if r.path[-1][2] <= t:
-                self.r_field_list.remove(r)
+        for r in self.rockets_list:
+            if r.path[-1][2] < t:
+                self.rockets_list.remove(r)
         return action
 
 
@@ -408,19 +413,19 @@ def my_main():
     my_intr = MyInterceptor()
     r_locs = i_locs = c_locs = []
     ang = score = 0
-    for stp in range(1000):
-        if stp % 1 == 0:
-            print ("step", stp, "score", score, "rockets", len(r_locs))
+    for stp in range(10000):
+        if stp % 100 == 0:
+            print("step", stp, "score", score, "rockets", len(r_locs))
         action_button = my_intr.calculate_action(r_locs, i_locs, c_locs, ang, score, stp)
         #if action_button == SHOOT:
-        #    action_button = STRAIGHT ## Rafi
+        #    action_button = STRAIGHT  ## Rafi
         r_locs, i_locs, c_locs, ang, score = Game_step(action_button)
-       # Draw()
-    print (score)
+    # Draw()
+    print(score)
+
 
 import cProfile
 
 if __name__ == '__main__':
-    #cProfile.run('my_main()')
-    my_main()
-
+    cProfile.run('my_main()')
+    #my_main()
