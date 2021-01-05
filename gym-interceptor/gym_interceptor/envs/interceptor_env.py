6af189e2885825378d17eba81465bb6bfb36c32d
Rafi Vivanti <vivanti@gmail.com>
2021-01-05 15:14:21 +0200

    unified prints order

--- gym-interceptor/gym_interceptor/envs/interceptor_env.py
+++ gym-interceptor/gym_interceptor/envs/interceptor_env.py
@@ -47,7 +47,7 @@ class InterceptorEnv(gym.Env):
         self.my_score += reward
 
         if self.stp % 100 == 0:
-            print("step", self.stp, "score", reward, "total score", self.my_score, "game score", self.game_score, "rockets", len(self.r_locs))
+            print("step", self.stp, "score", reward, "total score", self.my_score, "rockets", len(self.r_locs), "game score", self.game_score)
         self.stp += 1
         #next round: see what's new in the world
         self.r_locs, self.i_locs, self.c_locs, self.ang, self.game_score  = Interceptor_V2.Game_step(action)
@@ -57,8 +57,7 @@ class InterceptorEnv(gym.Env):
         self.state = game_map
         if self.stp >= self.max_steps:
             self.done = True
-            print("step", self.stp, "score", reward, "total score", self.my_score, "rockets", len(self.r_locs),
-                  "game score", self.game_score)
+            print("step", self.stp, "score", reward, "total score", self.my_score, "rockets", len(self.r_locs), "game score", self.game_score)
         else:
             self.done = False
         info = {
